0bc4f5f1f19089fc68d86b304836ca957559f951
Rachal Maren <rmaren@ev-q4.kzn.21-school.ru>
2022-03-31 14:54:25 +0300

    Теги как часть входных данных

--- lab6_pr5_site/app.js
+++ lab6_pr5_site/app.js
@@ -81,20 +81,25 @@ $(".tabs a span").toArray().forEach(function (element) {
 		}
 		else if ($element.parent().is(":nth-child(4)")) { 
 
-			$(".content").append(
-				'<input type="text" class="inp">'+
-				'<button class="btn">+</button>'
-			);
-			var newToDo;
-			$('.btn').on('click',function(){
-				newToDo= $('.inp').val();
-				if (newToDo!='') {
-					toDos.push( newToDo);
-					alert('Новое задание "'+newToDo+'" успешно добавлено!');
-					$('.inp').val("");
-				}
-			})
-		
+				var $input = $("<input>").addClass("description"), 
+					$inputLabel = $("<p>").text("Новая задача: "),
+					$tagInput = $("<input>").addClass("tags"),
+					$tagLabel = $("<p>").text("Тэги: "),
+					$button = $("<button>").text("+");
+				$button.on("click", function () {
+					var description = $input.val(),
+					// разделение в соответствии с запятыми
+					tags = $tagInput.val().split(","); 
+					toDoObjects.push({"description":description, "tags":tags}); 
+					// обновление toDos
+					toDos = toDoObjects.map(function (toDo) {
+						return toDo.description;
+					});
+					$input.val("");
+					$tagInput.val("");
+				});
+				$("main .content").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button); 
+			
 		}
 		return false;
 	})
